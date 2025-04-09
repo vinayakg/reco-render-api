c66a9a17ce0d6ca64652474c35c548287112792d
Ellakkiaa S <ellakkiaa@crayondata.com>
2025-04-09 22:28:59 +0530

    commented out few lines of code

--- api/main.py
+++ api/main.py
@@ -12,6 +12,7 @@ from fastapi.responses import FileResponse
 import os
 app = FastAPI()
 
+'''
 # Path to the favicon.ico file
 favicon_path = os.path.join(os.path.dirname(__file__), "static", "favicon.ico")
 
@@ -21,6 +22,7 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/favicon.ico", include_in_schema=False)
 async def favicon():
     return FileResponse(favicon_path)
+'''
 
 # --- Load Data on Startup ---
 user_profile_df = pd.read_excel("data/user_profile_data.xlsx")
